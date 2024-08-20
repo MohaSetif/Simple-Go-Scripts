474b792c78e0d2a8049040db79bb34da716ed730
MohaSetif <BouroubaMohamed004@gmail.com>
2024-08-20 22:24:06 +0100

    Using Random And Slices

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,7 +3,13 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
+
+func GetRandomName() string {
+	names := []string{"Khalil", "Aymen", "Anes", ""}
+	return names[rand.Intn(len(names))]
+}
 
 func Hello(name string) (string, error) {
 	if name == "" {
